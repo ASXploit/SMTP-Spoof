f856927e8642f2ad827f0cf2cc8f9fdfaf919775
Xploit <linkhto@Gmail.com>
2016-11-25 03:12:13 -0800

    Add files via upload

---
+++ Sendit.php
@@ -0,0 +1,27 @@
+<?php
+# <-- Created by Xploit -->
+
+/* --Sending Our Email-- */
+if(isset($_POST['subject']) && isset($_POST['message']) && isset($_POST['namespoof']) && isset($_POST['emailspoof'])) {
+	$DefaultEmail="linkhto@gmail.com";
+	$NameToSpoof=$_POST['namespoof'];
+	$EmailToSpoof=$_POST['emailspoof'];
+	$Subject=$_POST['subject'];
+	$Message=$_POST['message'];
+	$Message=wordwrap($Message, 70, "\r\n");
+	
+	/* Custom Headers */
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'From: ' . $NameToSpoof . '<' . $EmailToSpoof. '>' . "\r\n";
+
+	$Sendit=@mail( $DefaultEmail, $Subject, $Message, $headers );
+	
+	/* --Check if our mail has Sent-- */
+	if($Sendit) { echo "Sent!"; } else { echo "Failed.."; }
+} else {
+	
+	/* --Check if our inputs were filled-- */
+	if(!isset($Sendit)) { echo " Fill in all Inputs.."; }
+}
+?>
